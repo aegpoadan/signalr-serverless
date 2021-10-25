d11c055745409e172099f3cf68f0e6e20890e7cc
Aaron Psujek <ap058838@abcsupply.com>
2021-10-25 13:47:24 -0500

    Add project files.

---
+++ serverless-signalr-poc/PocHub.cs
@@ -0,0 +1,46 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using Microsoft.Azure.WebJobs.Extensions.SignalRService;
+using Microsoft.AspNetCore.SignalR;
+
+namespace serverless_signalr_poc
+{
+    public class PocHub : ServerlessHub
+    {
+        [FunctionName("negotiate")]
+        public SignalRConnectionInfo Negotiate([HttpTrigger(AuthorizationLevel.Anonymous)] HttpRequest req, ILogger logger)
+        {
+            var x = Negotiate(req.Headers["x-ms-signalr-user-id"]);
+            return x;
+        }
+
+        [FunctionName(nameof(OnConnected))]
+        public async Task OnConnected([SignalRTrigger] InvocationContext invocationContext, ILogger logger)
+        {
+            invocationContext.Headers.TryGetValue("Authorization", out var auth);
+            await Clients.All.SendAsync("connected", invocationContext.ConnectionId);
+        }
+
+        [FunctionName(nameof(JoinUserToGroup))]
+        public async Task JoinUserToGroup([SignalRTrigger] InvocationContext invocationContext, string userName, string groupName, ILogger logger)
+        {
+            await UserGroups.AddToGroupAsync(userName, groupName);
+            var userInGroup = await UserGroups.IsUserInGroup(userName, groupName);
+            await Clients.Group(groupName).SendAsync("userAdded", userName);
+        }
+
+        [FunctionName(nameof(JoinUserError))]
+        public async Task JoinUserError([SignalRTrigger] InvocationContext invocationContext, string userName, string groupName, ILogger logger)
+        {
+            await UserGroups.AddToGroupAsync(userName, groupName);
+            throw new Exception();
+        }
+    }
+}
